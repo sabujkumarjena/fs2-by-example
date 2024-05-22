cf6b0766a47be321d707743d9c270af56a0a6523
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-22 07:12:29 +0530

    reading csv file as fs2 stream

--- src/main/scala/com/allevite/CSVFileProcessing.scala
+++ src/main/scala/com/allevite/CSVFileProcessing.scala
@@ -1,6 +1,11 @@
 package com.allevite
 
 import cats.effect.{IO, IOApp}
+import fs2.io.file.{Files, Path}
+import fs2.text
+
+//import fs2.*
+//import fs2.io.*
 
 import scala.io.Source
 import scala.util.{Try, Using}
@@ -28,7 +33,20 @@ object CSVFileProcessing extends IOApp.Simple :
         .toList
     }.get
 
+  def readSetsAsStream(filename: String, predicate: MySet => Boolean = _ => true, limit: Int= 10): IO[List[MySet]] =
+    Files[IO]
+      .readAll(Path(filename))
+      .through(text.utf8.decode)
+      .through(text.lines)
+      .map(parseLine)
+      .unNone
+      .filter(predicate)
+      .take(limit) // Stream[IO, MySet]
+      .compile
+      .toList
+
 
   override def run: IO[Unit] =
     val filename = "sets.csv"
-    IO.println(readSets(filename))
+    //IO.println(readSets(filename))
+    readSetsAsStream(filename).map(println)
