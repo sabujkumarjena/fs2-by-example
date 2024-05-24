d4e3e0edf5b9525536716b499dc3be544500aee3
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-24 09:18:35 +0530

    debouncing

--- src/main/scala/com/allevite/chapt2_effectful/Timed.scala
+++ src/main/scala/com/allevite/chapt2_effectful/Timed.scala
@@ -8,6 +8,9 @@ object Timed extends IOApp.Simple:
 
   val drinkWater = Stream.iterateEval(1)(n => (IO.sleep(1500.millis) *> IO.println("Drink more water")).as(n+1))
 
+  // Debounce
+  val resizeEvents: Stream[IO, (Int, Int)] = Stream.iterate((0,0)) { case (w, h) => (w +1 , h +1)}.covary[IO]
+
   override def run: IO[Unit] =
     drinkWater.compile.drain
     drinkWater.timeout(5.second).compile.drain
@@ -25,3 +28,23 @@ object Timed extends IOApp.Simple:
       .interruptAfter(7.seconds)
       .compile
       .drain
+    resizeEvents
+      .evalMap { case (h, w) => IO.println(s"Resizing window to height $h and width $w")}
+      .interruptAfter(5.seconds)
+      .compile
+      .drain
+
+    resizeEvents
+      .debounce(200.millis)
+      .evalMap { case (h, w) => IO.println(s"Resizing window to height $h and width $w") }
+      .interruptAfter(5.seconds)
+      .compile
+      .drain
+
+    resizeEvents
+      .debounce(200.millis)
+      .evalTap { case (h, w) => IO.println(s"Resizing window to height $h and width $w") }
+      .interruptAfter(3.seconds)
+      .compile
+      .toList
+      .flatMap(IO.println)
