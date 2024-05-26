ec7164fe6cbc02111f15c5dc0a994c8b16221180
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-27 00:56:55 +0530

    Signaling termination with None

--- src/main/scala/com/allevite/chapt5_Communication/Queues.scala
+++ src/main/scala/com/allevite/chapt5_Communication/Queues.scala
@@ -32,6 +32,16 @@ object Queues extends IOApp.Simple :
       p.merge(c).interruptAfter(3.seconds) ++ Stream.eval(ref.get.flatMap(IO.println))
     }
   }
+
+  val q3 = Stream.eval(Queue.unbounded[IO, Option[Int]]).flatMap {q =>
+    val p = (Stream.range(0, 10).map(Some.apply) ++ Stream(None) ++ Stream(Some(11))) .evalMap(q.offer)
+    val c = Stream.fromQueueNoneTerminated(q).evalMap(i => IO.println(i))
+    c.merge(p)
+  }
+
   override def run: IO[Unit] =
     q1.compile.drain
     q2.compile.drain
+    q3.interruptAfter(5.seconds).compile.drain
+
+
