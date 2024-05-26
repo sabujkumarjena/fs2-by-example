8fbfc3b2c5452df067434ac91ec44cfc4ff2b9bb
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-27 02:13:31 +0530

    Simulating a server v2

--- src/main/scala/com/allevite/chapt5_Communication/Queues.scala
+++ src/main/scala/com/allevite/chapt5_Communication/Queues.scala
@@ -5,7 +5,10 @@ import cats.effect.{IO, IOApp, Ref}
 import fs2.*
 
 import scala.concurrent.duration.*
-import cats.effect.std.*
+import cats.effect.std.Queue
+import scala.util.Random
+
+import java.time.LocalDateTime
 
 object Queues extends IOApp.Simple :
 
@@ -39,9 +42,49 @@ object Queues extends IOApp.Simple :
     c.merge(p)
   }
 
+  trait Controller:
+    def postAccount(customerId: Long, accType: String, creationDate: LocalDateTime): IO[Unit]
+
+  class Server(controller: Controller):
+    def start():IO[Nothing] =
+      val prog =
+        for
+          randomWait <- IO(math.abs(Random.nextInt) % 500)
+          _ <- IO.sleep(randomWait.millis)
+          _ <- controller.postAccount(
+            customerId = Random.between(1L, 1000L),
+            accType =  if Random.nextBoolean then "SB" else "CA",
+            creationDate = LocalDateTime.now()
+          )
+        yield ()
+      prog.foreverM
+
+  object PrintController extends Controller:
+    override def postAccount(customerId: Long, accType: String, creationDate: LocalDateTime): IO[Unit] =
+      IO.println(s"Initiating account creation. Customer: $customerId, Account type: $accType Created: $creationDate")
+
+  case class CreateAccountData(customerId: Long, accType: String, creationDate: LocalDateTime)
+
+  class QueueController(queue: Queue[IO,CreateAccountData]) extends  Controller:
+    override def postAccount(customerId: Long, accType: String, creationDate: LocalDateTime): IO[Unit] =
+      queue.offer(CreateAccountData(customerId,accType, creationDate))
+
+  //Exercise
+  // Create a stream that emits the queue
+  // Create a stream for the server (started)
+  // Create a consumer stream which reads from the queue and prints the message
+  // Run everything concurrently
+
+  val myProg =
+    Stream.eval(Queue.unbounded[IO, CreateAccountData]).flatMap {q =>
+      val serverStream = Stream.eval(new Server(new QueueController(q)).start())
+      val consumerStream = Stream.fromQueueUnterminated(q).printlns
+      consumerStream.merge(serverStream)
+    }
   override def run: IO[Unit] =
     q1.compile.drain
     q2.compile.drain
     q3.interruptAfter(5.seconds).compile.drain
-
+    new Server(PrintController).start()
+    myProg.interruptAfter(5.seconds).compile.drain
 
