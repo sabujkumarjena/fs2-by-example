f7216e0d544f9dfeff62e3651fb5e6310966e719
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-27 00:34:52 +0530

    handling back pressure- queue acts as buffer

--- src/main/scala/com/allevite/chapt5_Communication/Queues.scala
+++ src/main/scala/com/allevite/chapt5_Communication/Queues.scala
@@ -20,5 +20,18 @@ object Queues extends IOApp.Simple :
       p.merge(c).interruptAfter(3.seconds) ++ Stream.eval(ref.get.flatMap(IO.println))
     }
   }
+  val q2: Stream[IO, Unit] = Stream.eval(Queue.unbounded[IO, Int]).flatMap { queue =>
+    Stream.eval(Ref.of[IO, Int](0)).flatMap { ref =>
+      val p = Stream.iterate(0)(_ + 1).covary[IO]
+        .evalMap(i => IO.println(f"Offering $i") >> queue.offer(i))
+        .drain //producer
+      val c = Stream.fromQueueUnterminated(queue)
+        .evalMap(i => ref.update(_ + i))
+        .metered(300.millis)
+        .drain //consumer
+      p.merge(c).interruptAfter(3.seconds) ++ Stream.eval(ref.get.flatMap(IO.println))
+    }
+  }
   override def run: IO[Unit] =
     q1.compile.drain
+    q2.compile.drain
