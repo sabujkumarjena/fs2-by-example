33f1c477ea5872437a2c4d14f6f4f8c911ce10e2
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-27 00:12:36 +0530

    Exercise- Car position tracker

--- src/main/scala/com/allevite/chapt5_Communication/Topics.scala
+++ src/main/scala/com/allevite/chapt5_Communication/Topics.scala
@@ -12,5 +12,42 @@ object Topics extends IOApp.Simple:
     val c2 = topic.subscribe(10).evalMap(i => IO.println(s"Read $i from c2")).drain
     Stream(p, c1, c2).parJoinUnbounded
   }
+
+  case class CarPos(carId: Long, lat: Double, lng: Double)
+  def createCar(carId: Long, topic: Topic[IO, CarPos]): Stream[IO, Nothing] =
+    Stream
+      .repeatEval(IO(CarPos(carId, Random.between(-90, 90), Random.between(-180, 180))))
+      .metered(1.second)
+      .through(topic.publish)
+      .drain
+  def createGoogleMapUpdater(topic: Topic[IO, CarPos]): Stream[IO, Nothing] =
+    topic
+      .subscribe(10)
+      .evalMap(pos => IO.println(f"Drawing position (${pos.lat}%.2f, ${pos.lng}%.2f) for car ${pos.carId} in map ... "))
+      .drain
+  //Exercise
+  def createDriverNotifier(
+      topic: Topic[IO, CarPos],
+      shouldNotify: CarPos => Boolean,
+      notify: CarPos => IO[Unit]
+                          ): Stream[IO, Nothing] =
+    topic
+      .subscribe(10)
+      .evalMap(pos => if shouldNotify(pos) then notify(pos) else IO.unit)
+      .drain
+
+  val t3: Stream[IO, Nothing] = Stream.eval(Topic[IO, CarPos]).flatMap { topic =>
+    val cars = Stream.range(1, 10).map(carId => createCar(carId, topic))
+    val googlMapUpdater = createGoogleMapUpdater(topic)
+    val driverNotifier = createDriverNotifier(
+      topic = topic,
+      shouldNotify = pos => pos.lat > 0,
+      notify = pos => IO.println(f"Car ${pos.carId}: you are above the equator ! (${pos.lat}%.2f), ${pos.lng}%.2f !")
+    )
+    (cars ++ Stream(googlMapUpdater, driverNotifier)).parJoinUnbounded
+  }
+
+
   override def run: IO[Unit] =
     t1.interruptAfter(3.seconds).compile.drain
+    t3.interruptAfter(5.seconds).compile.drain
