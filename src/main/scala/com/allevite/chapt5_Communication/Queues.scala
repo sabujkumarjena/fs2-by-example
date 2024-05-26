943d367be20599f298b2986219fba956204603bd
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-27 00:31:35 +0530

    queue

---
+++ src/main/scala/com/allevite/chapt5_Communication/Queues.scala
@@ -0,0 +1,24 @@
+package com.allevite.chapt5_Communication
+
+
+import cats.effect.{IO, IOApp, Ref}
+import fs2.*
+
+import scala.concurrent.duration.*
+import cats.effect.std.*
+
+object Queues extends IOApp.Simple :
+
+  val q1: Stream[IO, Unit] = Stream.eval(Queue.unbounded[IO, Int]).flatMap { queue =>
+    Stream.eval(Ref.of[IO, Int](0)).flatMap { ref =>
+      val p = Stream.iterate(0)(_ + 1).covary[IO]
+        .evalMap(i => IO.println(f"Offering $i") >> queue.offer(i))
+        .drain //producer
+      val c = Stream.fromQueueUnterminated(queue)
+        .evalMap(i => ref.update(_ + i))
+        .drain //consumer
+      p.merge(c).interruptAfter(3.seconds) ++ Stream.eval(ref.get.flatMap(IO.println))
+    }
+  }
+  override def run: IO[Unit] =
+    q1.compile.drain
